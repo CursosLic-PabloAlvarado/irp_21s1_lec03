a07f10b6c434b0cdade4d77f77cd0bed9aaa9b52
Pablo Alvarado-Moya <palvaradomoya@gmail.com>
2021-02-23 11:23:35 -0600

    Ejemplos para la lección 3

---
+++ linalg.m
@@ -0,0 +1,23 @@
+#!/usr/bin/octave-cli
+
+## Cargue los datos en del archivo 'escazu'.dat
+
+
+## Extraiga la columna de áreas y precios
+
+
+## Extraiga min-max de áreas y precios
+
+
+## Use las ecuaciones normales para calcular los parámetros de
+## regresión lineal
+
+
+## Grafique los puntos originales y la recta de regresión lineal con
+## al menos 100 puntos
+
+
+## ¿Cómo podemos modificar lo anterior para hacer una regresión
+## polinomial?
+
+
